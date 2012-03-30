8de49b57a48ce87306583c175762530a017c2312
Eric Bariaux <eric@openremote.org>
2012-03-30 11:34:48 +0000

    Added calls to returns devices, code sets and IRCommands
    git-svn-id: https://openremote.svn.beanstalkapp.com/beehive/workspace/ebariaux/IRService@70 2bd35c9c-1000-49a5-b779-596279832df3

--- src/org/openremote/irservice/service/ProntoService.java
+++ src/org/openremote/irservice/service/ProntoService.java
@@ -10,9 +10,15 @@ import java.util.zip.ZipFile;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.openremote.irservice.domain.BrandInfo;
+import org.openremote.irservice.domain.CodeSetInfo;
+import org.openremote.irservice.domain.DeviceInfo;
+import org.openremote.irservice.domain.IRCommandInfo;
 import org.xml.sax.SAXException;
 
 import com.tinsys.ir.database.Brand;
+import com.tinsys.ir.database.CodeSet;
+import com.tinsys.ir.database.Device;
+import com.tinsys.ir.database.IRCommand;
 import com.tinsys.ir.representations.IRCodeRepresentationFactory;
 import com.tinsys.ir.representations.pronto.NecIRCodeRepresentationHandler;
 import com.tinsys.ir.representations.pronto.RC5IRCodeRepresentationHandler;
@@ -28,7 +34,7 @@ public class ProntoService {
     return null;
   }
   
-  public ArrayList<BrandInfo> getBrands(String prontoFileHandle) {
+  public ArrayList<String> getBrands(String prontoFileHandle) {
     
     try {
       prontoFileParser.parseFile(new ZipFile(new File("/tmp/Pronto_" + prontoFileHandle + ".xcf"))); // TODO review name construction
@@ -47,19 +53,108 @@ public class ProntoService {
     }
     
     System.out.println("File parsed");
-    if (prontoFileParser != null) {
-      ArrayList<BrandInfo> brandInfo = new ArrayList<BrandInfo>();
-       List<Brand> brands = prontoFileParser.getBrands();
-       for (Brand brand : brands) {
-         System.out.println("Brand name " + brand.getBrandName());
-          brandInfo.add(new BrandInfo(brand.getBrandName()));
-       }
-       
-       return brandInfo;
-    } else {
-       return null;
-    }
+    ArrayList<String> brandInfo = new ArrayList<String>();
+     List<Brand> brands = prontoFileParser.getBrands();
+     for (Brand brand : brands) {
+       System.out.println("Brand name " + brand.getBrandName());
+        brandInfo.add(brand.getBrandName());
+     }
+     
+     return brandInfo;
  }
+  
+  public ArrayList<String> getDevices(String prontoFileHandle, String brandName) {
+    try {
+      prontoFileParser.parseFile(new ZipFile(new File("/tmp/Pronto_" + prontoFileHandle + ".xcf"))); // TODO review name construction
+    } catch (ZipException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (ParserConfigurationException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (SAXException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    ArrayList<String> deviceInfo = new ArrayList<String>();
+    Brand b = new Brand(brandName);
+    List<Device> devices = prontoFileParser.getDevices(b);
+    for (Device device : devices) {
+       deviceInfo.add(device.getModelName());
+    }
+    return deviceInfo;
+  }
+
+  public ArrayList<CodeSetInfo> getCodeSets(String prontoFileHandle, String brandName, String deviceName) {
+    try {
+      prontoFileParser.parseFile(new ZipFile(new File("/tmp/Pronto_" + prontoFileHandle + ".xcf"))); // TODO review name construction
+    } catch (ZipException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (ParserConfigurationException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (SAXException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    ArrayList<CodeSetInfo> codeSetInfo = new ArrayList<CodeSetInfo>();
+
+    Device d = new Device(new Brand(brandName), deviceName);
+    DeviceInfo di = new DeviceInfo(new BrandInfo(brandName), deviceName);
+
+    List<CodeSet> codeSets = prontoFileParser.getCodeSets(d);
+    int index = 0;
+    for (CodeSet codeSet : codeSets) {
+       codeSetInfo.add(new CodeSetInfo(di, codeSet.getDescription(), codeSet.getCategory(), index));
+       index++;
+    }
+    return codeSetInfo;
+  }
+
+  public ArrayList<IRCommandInfo> getIRCommands(String prontoFileHandle, String brandName, String deviceName, int index) {
+    try {
+      prontoFileParser.parseFile(new ZipFile(new File("/tmp/Pronto_" + prontoFileHandle + ".xcf"))); // TODO review name construction
+    } catch (ZipException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (ParserConfigurationException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (SAXException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    ArrayList<IRCommandInfo> iRCommandInfo = new ArrayList<IRCommandInfo>();
+    Device d = new Device(new Brand(brandName), deviceName);
+    List<IRCommand> iRcommands = prontoFileParser.getCodeSets(d).get(index).getIRCommands();
+
+    for (IRCommand irCommand : iRcommands) {
+        IRCommandInfo iRCI;
+        if (irCommand.getCode() != null) {
+           iRCI = new IRCommandInfo(irCommand.getName(), irCommand.getCode()
+                 .toString(), irCommand.getOriginalCodeString(),
+                 irCommand.getComment(), null); // TODO: do we really need codeset as last param
+        } else {
+           iRCI = new IRCommandInfo(irCommand.getName(), null,
+                 irCommand.getOriginalCodeString(), irCommand.getComment(), null); // TODO: do we really need codeset as last param
+        }
+        iRCommandInfo.add(iRCI);
+     }
+     return iRCommandInfo;
+  }
 
   public void setProntoFileParser(ProntoFileParser prontoFileParser) {
     this.prontoFileParser = prontoFileParser;
