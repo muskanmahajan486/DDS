157cb96eaf54cac6ab24f8b95f2dd92727a331d7
Marcus Redeker <marcus@openremote.org>
2012-05-04 21:38:20 +0000

    Added JPA and persisted domain objects
    git-svn-id: https://openremote.svn.beanstalkapp.com/beehive/workspace/mredeker/DeviceDiscoveryService@111 2bd35c9c-1000-49a5-b779-596279832df3

---
+++ src/org/openremote/devicediscovery/domain/Account.java
@@ -0,0 +1,106 @@
+/* OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2011, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.devicediscovery.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * The Class Account.
+ * 
+ * @author Marcus
+ */
+@Entity
+@Table(name = "account")
+public class Account extends BusinessEntity
+{
+
+  private static final long serialVersionUID = -5029444774173816237L;
+
+  /** The users. */
+  private List<User> users;
+
+  /** The devices. */
+  private List<DiscoveredDevice> discoverdDevices;
+
+  /**
+   * Instantiates a new account.
+   */
+  public Account()
+  {
+    discoverdDevices = new ArrayList<DiscoveredDevice>();
+  }
+
+  @OneToMany(mappedBy = "account")
+  public List<User> getUsers()
+  {
+    return users;
+  }
+
+  public void setUsers(List<User> users)
+  {
+    this.users = users;
+  }
+
+  @OneToMany(mappedBy = "account")
+  public List<DiscoveredDevice> getDiscoverdDevices()
+  {
+    return discoverdDevices;
+  }
+
+  public void setDiscoverdDevices(List<DiscoveredDevice> discoverdDevices)
+  {
+    this.discoverdDevices = discoverdDevices;
+  }
+
+  /**
+   * Adds a discovered device.
+   * 
+   * @param device
+   *          the discovered device
+   */
+  public void addDevice(DiscoveredDevice device)
+  {
+    discoverdDevices.add(device);
+  }
+
+  @Override
+  public int hashCode()
+  {
+    return (int) getOid();
+  }
+
+  @Override
+  public boolean equals(Object obj)
+  {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Account other = (Account) obj;
+    return other.getOid() == getOid();
+  }
+}
---
+++ src/org/openremote/devicediscovery/domain/DiscoveredDevice.java
@@ -0,0 +1,154 @@
+package org.openremote.devicediscovery.domain;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import flexjson.JSON;
+
+@Entity
+@Table(name = "discovered_device")
+public class DiscoveredDevice extends BusinessEntity
+{
+  private static final long serialVersionUID = -4324310503187625990L;
+
+  /** The device name. */
+  private String name;
+
+  /** The device's model. */
+  private String model;
+  
+  /** The OpenRemote protocol which is responsible to control this devices */
+  private String protocol;
+
+  /** The device type eg. Switch, Dimmer, Thermometer, TV, .... */
+  private String type;
+  
+  /** Is this device already used in the Building Modeler */ 
+  private Boolean used;
+
+  /** The device attrs. */
+  private List<DiscoveredDeviceAttr> deviceAttrs;
+
+  /** The account whose controller announced this device */
+  private Account account;
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public String getModel()
+  {
+    return model;
+  }
+
+  public void setModel(String model)
+  {
+    this.model = model;
+  }
+
+  public String getProtocol()
+  {
+    return protocol;
+  }
+
+  public void setProtocol(String protocol)
+  {
+    this.protocol = protocol;
+  }
+
+  public String getType()
+  {
+    return type;
+  }
+
+  public void setType(String type)
+  {
+    this.type = type;
+  }
+
+  /**
+   * Gets the device attrs.
+   * 
+   * @return the device attrs
+   */
+  @OneToMany(mappedBy = "discoveredDevice", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+  public List<DiscoveredDeviceAttr> getDeviceAttrs()
+  {
+    return deviceAttrs;
+  }
+
+  /**
+   * Sets the device attrs.
+   * 
+   * @param deviceAttrs
+   *          the new device attrs
+   */
+  public void setDeviceAttrs(List<DiscoveredDeviceAttr> deviceAttrs)
+  {
+    this.deviceAttrs = deviceAttrs;
+  }
+
+  /**
+   * Gets the account.
+   * 
+   * @return the account
+   */
+  @ManyToOne
+  @JSON(include = false)
+  public Account getAccount()
+  {
+    return account;
+  }
+
+  /**
+   * Sets the account.
+   * 
+   * @param account
+   *          the new account
+   */
+  public void setAccount(Account account)
+  {
+    this.account = account;
+  }
+
+  public Boolean getUsed()
+  {
+    return used;
+  }
+
+  public void setUsed(Boolean used)
+  {
+    this.used = used;
+  }
+
+  @Override
+  public int hashCode()
+  {
+    return (int) getOid();
+  }
+
+  @Override
+  public boolean equals(Object obj)
+  {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    DiscoveredDevice other = (DiscoveredDevice) obj;
+    return other.getOid() == getOid();
+  }
+}
